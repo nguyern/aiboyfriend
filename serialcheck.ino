31003bb7e298a80be877c9ac3589d40ce479945d
nguyern <nguyern@student.unimelb.edu.au>
2025-06-04 21:45:40 +1000

    Add files via upload

---
+++ serialcheck.ino
@@ -0,0 +1,22 @@
+const int TRIG_PIN = 7;
+const int ECHO_PIN = 6;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+}
+
+void loop() {
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+
+  long duration = pulseIn(ECHO_PIN, HIGH);
+  float distance = duration * 0.034 / 2.0;
+
+  Serial.println(distance);
+  delay(100); // smooth rate
+}
